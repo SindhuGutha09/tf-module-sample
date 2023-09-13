bc7ec9c4550ddfe0fe66df25972bcf7cbf9a2ed3
Sindh <sindhugutha88@gmail.com>
2023-09-13 17:47:59 -0500

    modified the required files in the project

--- data.tf
+++ data.tf
@@ -1,8 +1,5 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
   owners = ["342367090197"]
-}
-output "ami" {
-  value = data.aws_ami_ids.ami
 }
